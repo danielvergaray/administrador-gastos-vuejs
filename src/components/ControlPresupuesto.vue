08d5f6fad942d178585ca65c382d95260554cde4
Daniel Vergaray <danielvergarayescobar@gmail.com>
2024-04-18 15:57:26 -0500

    avance

---
+++ src/components/ControlPresupuesto.vue
@@ -0,0 +1,74 @@
+<script setup>
+import imagen from "../assets/img/grafico.jpg";
+</script>
+
+<template>
+  <div class="dos-columnas">
+    <div class="contenedor-grafico">
+      <img :src="imagen" />
+    </div>
+    <div class="contenedor-presupuesto">
+      <button class="reset-app">Resetear App</button>
+      <p><span>Presupuesto: </span>$0</p>
+      <p><span>Disponible: </span>$0</p>
+      <p><span>Gastado: </span>$0</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.dos-columnas {
+  display: flex;
+  flex-direction: column;
+}
+.dos-columnas > :first-child {
+  margin-bottom: 3rem; /* Como dos-columnas se repite y hay dos primeros hijos, usamos > para decir que solamente queremos aplicar estilos al primero de todos */
+}
+
+@media (min-width: 768px) {
+  .dos-columnas {
+    flex-direction: row;
+    gap: 4rem;
+    align-items: center;
+  }
+  .dos-columnas > :first-child {
+    margin-bottom: 0; /* Como dos-columnas se repite y hay dos primeros hijos, usamos > para decir que solamente queremos aplicar estilos al primero de todos */
+  }
+}
+.reset-app {
+  background-color: #db2777;
+  border: none;
+  padding: 1rem;
+  width: 100%;
+  color: var(--blanco);
+  font-weight: 900;
+  text-transform: uppercase;
+  border-radius: 1rem;
+  transition-property: background-color;
+  transition-duration: 300ms;
+}
+.reset-app:hover {
+  cursor: pointer;
+  background-color: #c11d67;
+}
+
+.contenedor-presupuesto {
+  width: 100%;
+}
+.contenedor-presupuesto p {
+  font-size: 2.4rem;
+  text-align: center;
+  color: var(--gris-oscuro);
+}
+
+@media (min-width: 768px) {
+  .contenedor-presupuesto p {
+    text-align: left;
+  }
+}
+
+.contenedor-presupuesto span {
+  font-weight: 900;
+  color: var(--azul);
+}
+</style>
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,86 @@
+<script setup>
+import {ref} from 'vue';
+import Alerta from './Alerta.vue';
+
+const presupuesto = ref (0)
+const error = ref('')
+
+const emit = defineEmits(['definir-presupuesto'])
+
+const definirPresupuesto=()=>{
+if (presupuesto.value <= 0 || presupuesto.value===''){
+  error.value= 'Presupuesto no valido'
+
+  setTimeout(()=>{
+    error.value= ''
+  }, 3000)
+
+  return
+
+}
+emit('definir-presupuesto', presupuesto.value)
+}
+</script>
+<template>
+  <form class="presupuesto" @submit.prevent="definirPresupuesto">
+
+    <Alerta v-if="error">
+      
+      {{ error }}
+    </Alerta>
+
+    <div class="campo">
+      <label for="nuevo-presupuesto">Definir Presupuesto</label>
+      <input
+        id="nuevo-presupuesto"
+        class="nuevo-presupuesto"
+        placeholder="Añade tu presupuesto"
+        type="number"
+        v-model="presupuesto"
+        min="0"
+      />
+    </div>
+    <input class="presupuesto" type="submit" value="Definir Presupuesto" />
+  </form>
+</template>
+
+<style scoped>
+.presupuesto {
+  width: 100%;
+  
+}
+.campo{
+  display: grid;
+  gap: 2rem;
+}
+.presupuesto label{
+  font-size: 2.2rem;
+  text-align: center;
+  color: var(--azul);
+}
+.presupuesto input[type="number"]{
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  text-align: center;
+  font-size: 2.2rem;
+}
+.presupuesto input[type="submit"]{
+  background-color: var(--azul);
+  padding: 1rem;
+  border: none;
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 2rem;
+  color: var(--blanco);
+  font-weight: 900;
+  width: 100%;
+  transition: background-color 300ms ease;
+}
+.presupuesto input[type="submit"]:hover{
+  background-color: #1048a4;
+  cursor: pointer;
+}
+
+</style>
